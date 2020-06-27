77dfc5205048db5d29a1c811709a3af55f81caf1
Rajil1213 <imrb1213@gmail.com>
2020-06-27 18:14:53 +0545

    Implemented Challenge 1 of Set 1

---
+++ hex2base64.py
@@ -0,0 +1,19 @@
+from sys import argv, exit
+from utils import BinaryMap
+
+def main():
+
+    if not len(argv) == 2:
+        print("Usage: python3 BinaryMap.py <hex string>")
+        exit(1)
+
+    hexValue = argv[1]
+    mapping = BinaryMap()
+    # convert hex to binary
+    binaryValue = mapping.hexToBinary(hexValue)
+    # convert binary to base64
+    base64Value = mapping.binaryToBase64(binaryValue)
+
+    print(f"Base64 Equivalent: { base64Value }")
+
+main()
