9b3853a43ff1d28f1404238f373414c4de205048
Rajil1213 <imrb1213@gmail.com>
2020-06-27 19:52:12 +0545

    Added docstring to xor

--- utils/BinaryOps.py
+++ utils/BinaryOps.py
@@ -4,6 +4,15 @@ class BinaryOps:
 
     @classmethod
     def xor(cls, arg1, arg2):
+        """performs bitwise XOR on bits of arg1 and arg2
+
+        Args:
+            arg1 (string): first operand
+            arg2 (string): second operand
+
+        Returns:
+            string: the result of `arg1` bitwise-XOR `arg2`
+        """
 
         if not len(arg1) == len(arg2):
             print("Bit lengths of arguments not equal")
