b82f054832db62e1cc469c6b431ecb26ce639362
Rajil1213 <imrb1213@gmail.com>
2020-06-28 19:30:30 +0545

    Completed Challenge 5 of Set 1

---
+++ challenge5.py
@@ -0,0 +1,28 @@
+from utils.XOR import AsciiXOR
+
+def main():
+
+    # Example of Encryption According to the Challenge Specification
+    key = "ICE"
+    plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    print("Example:")
+    print(f"Plaintext: { plaintext }")
+    print(f"Key: { key }")
+    while True:
+        
+        encode = AsciiXOR(plaintext)
+
+        result = encode.repeatKeyEncrypt(key)
+        print(f"Ciphertext: { result }")
+        
+        again = input("Enter 'y' To Encrypt Another: ")
+        if not again.lower() == 'y':
+            break
+        
+        plaintext = input("Enter Text to Encrypt: ")
+        key = input("Enter Key: ")
+
+if __name__ == "__main__":
+    main()
+
+
